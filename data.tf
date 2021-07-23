7acf3c4452849abe362e8517e60d9004118e809c
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-07-23 13:45:18 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,15 +1,32 @@
-data "aws_vpc" "default"{
+locals {
+ name ="nyl-${var.lob}-${var.env}-${var.application}-${var.emr_cluster}
+}
+
+data "aws_ami" "ami_os_type"{
 filter{
  name = "tag:Name"
-values = ["nyl-${var.lob}-${var.env}-vpc"]
+ values = ["${var.lob}-${var.env}-amzlinux2
 }
 }
 
-data "aws_subnet_ids" "data"{
-vpc_id = data.aws_vpc.default.id
-  tags={
- tier ="data"
+data "aws_vpc" "default"{
+filter{
+ name = "tag:Name"
+ values = ["nyl-${var.lob}-${var.env}-vpc"]
 }
+}
+
+data "aws_subnet_ids" "data" {
+    vpc_id = data.aws_vpc.this.id
+
+    tags={
+        tier = "compute"
+    }
+}
+
+data "aws_subnet" "compute" {
+for_each = data.aws_subnet_ids.data.ids
+id =each.value
 }
 
 data "aws_security_group" "default"{
